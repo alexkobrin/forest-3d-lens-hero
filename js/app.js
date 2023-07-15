738147c74e7fdf3ec494a2ebde84f11bb495f77d
Aliaksei <storyday2012@gmail.com>
2023-07-15 18:50:30 +0200

    update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 document.addEventListener('mousemove', e => {
   Object.assign(document.documentElement, {
     style: `
-    --move-x : ${(e.clientX - window.innerWidth / 2) * -.05}deg;
-    --move-y : ${(e.clientY  - window.innerHeight / 2) * -.05}deg;
+    --move-x : ${(e.clientX - window.innerWidth / 2) * -.0035}deg;
+    --move-y : ${(e.clientY  - window.innerHeight / 2) * -.0035}deg;
     `
   })
 })
